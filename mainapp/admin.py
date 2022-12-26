6c50d4ac9b16cde59a44ccd0df5fcbf2e2ce280a
Natalia <tsepi2014@hotmail.com>
2022-12-26 14:08:58 +0100

    lesson_1 Add admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from mainapp import models as mainapp_models
+
+
+@admin.register(mainapp_models.User)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["last_name", "first_name", "email"]
+    ordering = ["last_name"]
