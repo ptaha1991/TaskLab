ced941c5148b47d4313664ae88245455e7570930
Natalia <84445480+ptaha1991@users.noreply.github.com>
2022-12-27 21:23:56 +0100

    Lesson 1 (#1)
    lesson_1 ready

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp import models as mainapp_models
+
+
+@admin.register(mainapp_models.User)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["last_name", "first_name", "email"]
+    ordering = ["last_name"]
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,11 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday_year = models.PositiveIntegerField(blank=True, null=True)
+    email = models.CharField(max_length=256, unique=True)
