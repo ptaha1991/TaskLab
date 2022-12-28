24c164efc230107717786797e0caffb9742af318
Natalia <tsepi2014@hotmail.com>
2022-12-28 22:03:46 +0100

    created react app and userlist

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UserList from './components/User.js'
+import axios from 'axios'
+
+class App extends React.Component {
+   constructor(props) {
+       super(props)
+       this.state = {
+           'users': []
+       }
+   }
+
+//   componentDidMount() {
+//        const users = [
+//            {
+//                'first_name': 'Фёдор',
+//                'last_name': 'Достоевский',
+//                'birthday_year': 1821
+//            },
+//            {   'first_name': 'Александр',
+//                'last_name': 'Грин',
+//                'birthday_year': 1880
+//            },
+//        ]
+//       this.setState(
+//           {
+//               'users': users
+//           }
+//       )
+//   }
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users')
+            .then(response => {
+                const users = response.data
+                    this.setState(
+                    {
+                        'users': users
+                    }
+                    )
+            }).catch(error => console.log(error))
+    }
+
+   render () {
+       return (
+           <div>
+               <UserList users={this.state.users} />
+           </div>
+       )
+    }
+}
+export default App;
---
+++ frontend/src/components/User.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const UserItem = ({user}) => {
+    return (
+    <tr>
+        <td>
+            {user.first_name}
+        </td>
+        <td>
+            {user.last_name}
+        </td>
+        <td>
+            {user.birthday_year}
+        </td>
+    </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <th>
+                First name
+            </th>
+            <th>
+                Last Name
+            </th>
+            <th>
+                Birthday year
+            </th>
+           {users.map((user) => <UserItem user={user} />)}
+        </table>
+    )
+}
+
+export default UserList
