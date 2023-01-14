fb378f53a683525d30b131379a3f445070bc7585
Natalia <tsepi2014@hotmail.com>
2023-01-14 14:26:28 +0100

    lesson_4 pagination

--- config/settings.py
+++ config/settings.py
@@ -132,4 +132,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-REST_FRAMEWORK = {"DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]}
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+}
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,4 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import ProjectFilter, TodoFilter
@@ -5,16 +6,26 @@ from .models import Project, Todo
 from .serializers import ProjectModelSerializer, TodoModelSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
     filterset_class = ProjectFilter
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
     filterset_class = TodoFilter
+    pagination_class = TodoLimitOffsetPagination
     # filterset_fields = {
     #     'project': ['exact'],
     #     'created_at': ['gte', 'lte']
