23b85f89cbf4b9f9c821feea4b7660f265b6fd23
Natalia <tsepi2014@hotmail.com>
2023-01-14 17:50:18 +0100

    lesson_4 viewsets

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -6,4 +6,5 @@ from .models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "email")
+        # fields = ("first_name", "last_name", "email")
+        fields = "__all__"
