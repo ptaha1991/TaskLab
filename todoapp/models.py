f5df02b0dbf6d350f61323c18a33e9ec021f77ff
Natalia <84445480+ptaha1991@users.noreply.github.com>
2023-01-14 11:24:42 +0100

    lesson 3 ready (#3)

---
+++ todoapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from mainapp.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=64)
+    link = models.URLField(blank=True, null=True)
+    users = models.ManyToManyField(User)
+
+
+class Todo(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
